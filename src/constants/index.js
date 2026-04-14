f597541afe6d65a87c72af90aad938d650dc65df
Ameer-Hamza-Malik <hm978835@gmail.com>
2026-04-14 16:36:04 +0500

    fu

--- src/constants/index.js
+++ src/constants/index.js
@@ -202,7 +202,7 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Car fucking",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
